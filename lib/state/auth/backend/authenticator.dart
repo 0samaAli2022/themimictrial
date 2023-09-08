cfcad1b600582f167b8fe872596203dbd0841c35
osama <osamaali31121999@gmail.com>
2023-09-08 23:08:20 +0300

    some ios config done

--- lib/state/auth/backend/authenticator.dart
+++ lib/state/auth/backend/authenticator.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:themimictrial/state/auth/constants/constants.dart';
 import 'package:themimictrial/state/auth/models/auth_result.dart';
@@ -55,38 +54,38 @@ class Authenticator {
     }
   }
 
-  Future<AuthResult> loginWithFacebook() async {
-    final loginResult = await FacebookAuth.instance.login();
-    final token = loginResult.accessToken?.token;
-    if (token == null) {
-      // user has aborted the login process
-      return AuthResult.aborted;
-    }
-    final oauthCredentials = FacebookAuthProvider.credential(token);
-    try {
-      await FirebaseAuth.instance.signInWithCredential(
-        oauthCredentials,
-      );
-      return AuthResult.success;
-    } on FirebaseAuthException catch (e) {
-      final email = e.email;
-      final credential = e.credential;
-      if (e.code == Constants.accountExistsWithDifferentCredential &&
-          email != null &&
-          credential != null) {
-        final providers =
-            await FirebaseAuth.instance.fetchSignInMethodsForEmail(
-          email,
-        );
-        if (providers.contains(Constants.googleCom)) {
-          await loginWithGoogle();
-          FirebaseAuth.instance.currentUser?.linkWithCredential(
-            credential,
-          );
-        }
-        return AuthResult.success;
-      }
-      return AuthResult.failure;
-    }
-  }
+  // Future<AuthResult> loginWithFacebook() async {
+  //   final loginResult = await FacebookAuth.instance.login();
+  //   final token = loginResult.accessToken?.token;
+  //   if (token == null) {
+  //     // user has aborted the login process
+  //     return AuthResult.aborted;
+  //   }
+  //   final oauthCredentials = FacebookAuthProvider.credential(token);
+  //   try {
+  //     await FirebaseAuth.instance.signInWithCredential(
+  //       oauthCredentials,
+  //     );
+  //     return AuthResult.success;
+  //   } on FirebaseAuthException catch (e) {
+  //     final email = e.email;
+  //     final credential = e.credential;
+  //     if (e.code == Constants.accountExistsWithDifferentCredential &&
+  //         email != null &&
+  //         credential != null) {
+  //       final providers =
+  //           await FirebaseAuth.instance.fetchSignInMethodsForEmail(
+  //         email,
+  //       );
+  //       if (providers.contains(Constants.googleCom)) {
+  //         await loginWithGoogle();
+  //         FirebaseAuth.instance.currentUser?.linkWithCredential(
+  //           credential,
+  //         );
+  //       }
+  //       return AuthResult.success;
+  //     }
+  //     return AuthResult.failure;
+  //   }
+  //}
 }
--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -7,7 +7,6 @@ import 'package:themimictrial/views/components/constants/strings.dart' as str;
 import 'package:themimictrial/views/constants/app_colors.dart';
 import 'package:themimictrial/views/constants/strings.dart';
 import 'package:themimictrial/views/login/divider_with_margins.dart';
-import 'package:themimictrial/views/login/facebook_button.dart';
 import 'package:themimictrial/views/login/google_button.dart';
 import 'package:themimictrial/views/login/login_view_signup_links.dart';
 
@@ -47,23 +46,23 @@ class LoginView extends ConsumerWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: AppColors.loginButtonColor,
-                      foregroundColor: AppColors.loginButtonTextColor,
-                    ),
-                    onPressed: () async {
-                      loadingScrean(context, str.Strings.loading);
-                      await ref
-                          .read(authStateProvider.notifier)
-                          .loginWithFacebook();
-                      Navigator.of(context).pop();
-                    },
-                    child: const FacebookButton(),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
+                  // TextButton(
+                  //   style: TextButton.styleFrom(
+                  //     backgroundColor: AppColors.loginButtonColor,
+                  //     foregroundColor: AppColors.loginButtonTextColor,
+                  //   ),
+                  //   onPressed: () async {
+                  //     loadingScrean(context, str.Strings.loading);
+                  //     await ref
+                  //         .read(authStateProvider.notifier)
+                  //         .loginWithFacebook();
+                  //     Navigator.of(context).pop();
+                  //   },
+                  //   child: const FacebookButton(),
+                  // ),
+                  // const SizedBox(
+                  //   height: 20,
+                  // ),
                   TextButton(
                     style: TextButton.styleFrom(
                       backgroundColor: AppColors.loginButtonColor,
